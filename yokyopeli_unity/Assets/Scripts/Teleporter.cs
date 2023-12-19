b26f825506c5b4e1b436a28d58f6cc4a0e18f7e6
modusmogulus <39548121+modusmogulus@users.noreply.github.com>
2023-12-19 10:58:07 +0200

    matikantunti type beat

--- yokyopeli_unity/Assets/Scripts/Teleporter.cs
+++ yokyopeli_unity/Assets/Scripts/Teleporter.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using Q3Movement;
+using EasyTransition;
+
 public class TeleportTrigger : MonoBehaviour
 {
     public Transform teleportDestination;
+    public TransitionSettings transition;
     private Q3Movement.Q3PlayerController controller;
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +16,8 @@ public class TeleportTrigger : MonoBehaviour
             // Teleport the player to the destination
             controller = other.GetComponent<Q3Movement.Q3PlayerController>();
             controller.Teleport(teleportDestination.transform.position);
+            TransitionManager.Instance().Transition(transition, 0.0f);
+
     }
     }
     private void OnDrawGizmos()
